0f845b769ac250441b8db039068f5a583d49ca25
wulijun <wulijun@walkclass.com>
2019-02-21 17:05:24 +0800

    解决：spring 通配符的匹配很全面, 但无法找到元素 'context:component-scan' 的声明 原因：在xml文件里xsd没有配置

--- src/main/java/com/gaussic/controller/MainController.java
+++ src/main/java/com/gaussic/controller/MainController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller     //明确地定义该类为处理请求的Controller类；
 public class MainController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)    //用于定义一个请求映射，value为请求的url，值为 / 说明，该请求首页请求，method用以指定该请求类型，一般为get和post；
     public String index() {
         return "index";
     }
